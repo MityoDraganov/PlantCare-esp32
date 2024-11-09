87230636ce7688ce15cb91180596628b5715d3c3
Mityo Draganov <mityodraganow@gmail.com>
2024-11-09 04:48:01 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-#include <WebServer.h>
-#include <DNSServer.h>
-#include "websocket.h"
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
 #include "utils/Multiplexer/Multiplexer.h"
@@ -11,132 +8,20 @@
 #include <Wire.h>
 #include "drivers/SensorManager/SensorManager.h"
 #include "config.json.h"
-#include <ArduinoWebsockets.h>
 #include <Ticker.h>
 
 EEPROMUtil eepromUtil(0x50);
-bool isWebSocketConnected = false;
-
-using namespace websockets;
-
-WebServer server(80);
 ModuleUtil moduleUtil(32);
 String generateSerialNumber(int length);
-bool webSocketConnected = false;
-unsigned long lastReconnectAttempt = 0;
-const unsigned long reconnectInterval = 5000;
 
 const int EEPROM_SSID_ADDR = 0;
 const int EEPROM_PASS_ADDR = 64;
 const int EEPROM_MAX_LEN = 32;
+const int NUM_MUX_CHANNELS = 4;
 
 const int channelToGPIO[] = {32, 33, 34, 35};
 
-WebsocketsClient client;
-
-Ticker keepAliveTicker;
-
-String getSSIDs()
-{
-    String ssids = "";
-    WiFi.disconnect();
-    int n = WiFi.scanNetworks();
-    if (n == 0)
-    {
-        ssids += "<option>No networks found</option>";
-    }
-    else
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            ssids += "<option value=\"" + WiFi.SSID(i) + "\">" + WiFi.SSID(i) + "</option>";
-        }
-    }
-    return ssids;
-}
-
 DynamicJsonDocument jsonDoc(1024);
-
-// HTML content to be served
-const char *index_html = R"rawliteral(
-<!DOCTYPE html>
-<html>
-<head><title>ESP32 Wi-Fi Config</title></head>
-<body>
-<h1>ESP32 Wi-Fi Configuration</h1>
-<form action="/save" method="POST">
-    <label for="ssid">Wi-Fi SSID:</label>
-    <select id="ssid" name="ssid">
-        <!--SSIDS_PLACEHOLDER-->
-    </select><br><br>
-    <label for="password">Wi-Fi Password:</label>
-    <input type="password" id="password" name="password" required><br><br>
-    <input type="submit" value="Save">
-</form>
-</body>
-</html>
-)rawliteral";
-
-void handleRoot()
-{
-    String html = index_html;
-    html.replace("<!--SSIDS_PLACEHOLDER-->", getSSIDs());
-    server.send(200, "text/html", html);
-}
-
-void connectToWiFi(const String &ssid, const String &password)
-{
-    WiFi.begin(ssid.c_str(), password.c_str());
-
-    Serial.print("Connecting to Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("Connected to Wi-Fi!");
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-}
-
-void handleSave()
-{
-    String ssid = server.arg("ssid");
-    String password = server.arg("password");
-
-    // Attempt to connect to the selected Wi-Fi network
-    connectToWiFi(ssid, password);
-
-    String response = "Trying to connect to " + ssid + "...";
-    server.send(200, "text/plain", response);
-}
-
-void setupOTA()
-{
-    ArduinoOTA.onStart([]()
-                       {
-        String type = (ArduinoOTA.getCommand() == U_FLASH) ? "sketch" : "filesystem";
-        Serial.println("Start updating " + type); });
-
-    ArduinoOTA.onEnd([]()
-                     { Serial.println("\nEnd"); });
-
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
-                          { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
-
-    ArduinoOTA.onError([](ota_error_t error)
-                       {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR) Serial.println("End Failed"); });
-
-    ArduinoOTA.setPort(8266);
-    ArduinoOTA.begin(); // Initialize OTA service
-}
 
 void setup()
 {
@@ -145,56 +30,35 @@ void setup()
         Serial.println("SPIFFS Mount Failed");
         return;
     }
-    client.close();
     Serial.begin(115200);
     while (!Serial)
     {
-        ; // Wait for serial port to connect. Needed for native USB
-    }
-    DeserializationError error = deserializeJson(jsonDoc, configJson);
-    if (error)
-    {
-        Serial.println("Failed to parse JSON");
-        return;
+        ;
     }
 
     eepromUtil.begin();
     Wire.begin();
-    WiFi.mode(WIFI_STA);
 
-    Serial.println("Starting AP mode...");
-    WiFi.softAP("ESP32_Config_AP");
-    Serial.print("AP IP Address: ");
-    Serial.println(WiFi.softAPIP());
+    // Generate serial numbers for each mux channel
+    for (int i = 0; i < NUM_MUX_CHANNELS; i++)
+    {
+        String serialNumber = generateSerialNumber(16);
+        eepromUtil.writeStringExternal(0, serialNumber, 16, i);
+    }
 
-    moduleUtil.readModules();
-
-
-    SensorManager::initializeSensors();
-
-    server.on("/", HTTP_GET, handleRoot);
-    server.on("/save", HTTP_POST, handleSave);
-    server.onNotFound(handleRoot);
-
-    server.begin();
-
-    setupOTA();
-    keepAliveTicker.attach(5, sendKeepAlive);
+    // Read the serial numbers from EEPROM
+    for (int i = 0; i < NUM_MUX_CHANNELS; i++)
+    {
+        String serialNumber = eepromUtil.readStringExternal(0, 16, i);
+        Serial.print("Serial number for channel ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(serialNumber);
+    }
 }
 
 void loop()
 {
-    client.ping();
-    ArduinoOTA.handle();
-    server.handleClient();
-    moduleUtil.readModules();
-
-    if (WiFi.isConnected() && !isWebSocketConnected)
-    {
-        connectToWebSocket("ws://192.168.0.144:8080/v1/pots/?token=pot_1");
-    }
-
-    client.poll();
 }
 
 std::mt19937 generator;
